9eb287ec99e9cadb9f068836576dc31db9beaf6f
tejth <ptejendra91@gmail.com>
2024-06-28 12:22:46 +0530

    create home section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="container-fluid home">
+        <div className="container home-content">
+          <h1>Hi I'm a </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
