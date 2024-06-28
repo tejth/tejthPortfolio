b3a6e54696e8aa08f9010f80d6a76c0e04a54797
tejth <ptejendra91@gmail.com>
2024-06-28 12:41:03 +0530

    add text effect using npm typwriter

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
@@ -7,6 +8,15 @@ function Home() {
       <div className="container-fluid home">
         <div className="container home-content">
           <h1>Hi I'm a </h1>
+          <h2>
+            <Typewriter
+              options={{
+                strings: ["Frontend Developer!", "Web Developer!"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
         </div>
       </div>
     </>
