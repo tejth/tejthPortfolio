68aaeb94934cbedec7005112af2c599dc646372e
tejth <ptejendra91@gmail.com>
2024-06-30 02:42:25 +0530

    add colors to techstack

--- src/Components/TechStack/TechStack.jsx
+++ src/Components/TechStack/TechStack.jsx
@@ -55,6 +55,26 @@ function TechStack() {
       name: "C++",
     },
   ];
+
+  const colors = [
+    "#0088FE",
+    "#48088A",
+    "#8181F7",
+    "#FE2EF7",
+    "#585858",
+    "#00C79F",
+    "#FFBB24",
+    "#FF1042",
+    "#0088FE",
+    "#00C49F",
+    "#FFBB28",
+    "#FF8042",
+    "#001CCE",
+    "#00C79F",
+    "#FFBB24",
+    "#FF1042",
+    "#3498DB ",
+  ];
   return (
     <>
       <div className="container techstack-section">
@@ -65,8 +85,14 @@ function TechStack() {
 
         <div className="row">
           {data.map((item, index) => (
-            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12">
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 " key={index}>
               <div className="tech-content">
+                <span
+                  className="tech-number"
+                  style={{ backgroundColor: colors[index] }}
+                >
+                  {index + 1}
+                </span>
                 <p>{item.name}</p>
               </div>
             </div>
