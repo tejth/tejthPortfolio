a070ac1b47a53f15017716623c330868bfcffe60
tejth <ptejendra91@gmail.com>
2024-07-06 08:37:22 +0530

    fix responsiveness

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -54,17 +54,14 @@ function About() {
                 Hey there! I'm Tejendra, a passionate BTech student with a knack
                 for crafting delightful user experiences through frontend
                 development. Currently immersed in the dynamic world of web
-                technologies, I thrive on turning ideas into interactive,
-                pixel-perfect realities. Driven by a curiosity for blending
-                creativity with functionality, I specialize in harnessing HTML,
-                CSS, and JavaScript to breathe life into web interfaces. Whether
-                it's responsive designs that adapt seamlessly across devices or
-                optimizing user journeys through intuitive UX/UI, I am dedicated
-                to creating experiences that leave a lasting impression. Beyond
-                coding, I am always exploring the latest trends in frontend
-                frameworks like React and Vue.js, constantly refining my skills
-                to stay at the forefront of innovation. Let's connect and
-                discuss how we can elevate digital experiences together!
+                technologies. I specialize in harnessing HTML, CSS, and
+                JavaScript to breathe life into web interfaces. Whether it's
+                responsive designs that adapt seamlessly across devices or
+                optimizing user journeys through intuitive UX/UI. Beyond coding,
+                I am always exploring the latest trends in frontend frameworks
+                like React.. constantly refining my skills. Currenly Learning
+                Backend with NodeJs. Let's connect and discuss how we can
+                elevate digital experiences together!
               </p>
             </Fade>
           </div>
